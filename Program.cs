4a340368c7cd8e07060129a869c6becc44a13a31
Spenezanni <gustavospenezanni@gmail.com>
2022-11-16 16:59:21 -0300

    Testing .Net

--- Program.cs
+++ Program.cs
@@ -3,17 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World 2!");
 app.MapGet("/user", () => "Luiz Gustavo");
 app.MapGet("/userCom", () => new {Name = "Luiz Gustavo", Age = 35});
 app.MapGet("/addHeader", (HttpResponse response) => response.Headers.Add("Teste", "Luiz Gustavo"));
 app.MapGet("/addHeader2", (HttpResponse resp) => {
     resp.Headers.Add("test", "Luiz Gustavo");
-    return new {name = "Luiz Gustavo", AggregateException = 35};
+    return new {name = "Luiz Gustavo", Age = 35};
 });
 
-app.MapPost("/saveProduc", (Product prod) => {
-    return prod.Code +  " - " + prod.Name;
+app.MapPost("/saveProduct", (Product prod) => {
+    ProductRepository.Add(prod);
 });
 
 app.MapGet("/getProduct", ( [FromQuery] string dateStart, string dateEnd) => {
@@ -21,14 +20,53 @@ app.MapGet("/getProduct", ( [FromQuery] string dateStart, string dateEnd) => {
 });
 
 app.MapGet("/getProduct/{code}", ([FromRoute] string code) => {
-    return code;
+    var product = ProductRepository.GetByCode(code);
+    return product;
+});
+
+app.MapGet("/getProducByHeader", (HttpRequest req) => {
+    return req.Headers["product-code"].ToString();
+});
+
+app.MapPut("/editProduct", (Product prod) => {
+   var prodSave = ProductRepository.GetByCode(prod.Code);
+   prodSave.Name = prod.Name;
+   //ProductRepository.Add(prodSave);
+   //return prodSave;
+});
+
+app.MapDelete("/deleteProduct/{code}", ([FromRoute] string code) => {
+    var prodget = ProductRepository.GetByCode(code);
+    ProductRepository.Remove(prodget);
 });
 
 app.Run();
+
 
 public class Product{
     public String Code { get; set; }
     public String Name {get; set;} 
 
+}
 
+
+public static class ProductRepository {
+    public static List<Product> Products {get; set;}
+
+    public static void Add(Product prop){
+        if(Products == null){
+            Products = new List<Product>();
+            Products.Add(prop);
+        }
+    }
+    
+    public static Product GetByCode(string code){
+        return Products.FirstOrDefault(p => p.Code == code);
+   
+    }
+
+    public static void Remove(Product prop) {
+        Products.Remove(prop);
+    }
+    
 }
